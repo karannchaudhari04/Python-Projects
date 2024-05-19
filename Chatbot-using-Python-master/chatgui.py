7377f3bc8b9f060fddc914b996876e6cd1d5ce7e
karannchaudhari04 <karanchaudhari34804@gmail.com>
2024-05-19 23:07:41 +0530

    initial commit

--- Chatbot-using-Python-master/chatgui.py
+++ Chatbot-using-Python-master/chatgui.py
@@ -1,4 +1,4 @@
-# https://github.com/karannchaudhari04/Chatbot-using-Python-master
+# https://github.com/karannchaudhari04/Python/tree/main/Chatbot-using-Python-master
 
 import nltk
 from nltk.stem import WordNetLemmatizer
